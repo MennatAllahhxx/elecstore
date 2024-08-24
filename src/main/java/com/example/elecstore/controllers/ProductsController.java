0b4b95c8592915cbc8c69ac26b1acba543c44d9d
Menna Hassan <h.menna85@gmail.com>
2024-08-24 18:04:00 +0300

    edit products

--- src/main/java/com/example/elecstore/controllers/ProductsController.java
+++ src/main/java/com/example/elecstore/controllers/ProductsController.java
@@ -10,10 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.InputStream;
@@ -47,6 +44,81 @@ public class ProductsController {
 
     @PostMapping("/create")
     public String createProduct(@Valid @ModelAttribute ProductDto productDto, BindingResult result) {
+        if (productDto.getImageFile().isEmpty()) {
+            result.addError(new FieldError("productDto", "imageFile", "The image file is required"));
+        }
+
+        if (result.hasErrors()) {
+            return "products/CreateProduct";
+        }
+
+        MultipartFile image = productDto.getImageFile();
+        Date createdAt = new Date();
+        String storageFileName = createdAt.getTime() + "_" + image.getOriginalFilename();
+
+        try {
+            String uploadDir = "public/images/";
+            Path uploadPath = Paths.get(uploadDir);
+
+            if(!Files.exists(uploadPath)) {
+                Files.createDirectories(uploadPath);
+            }
+
+            try (InputStream inputStream = image.getInputStream()) {
+                Files.copy(inputStream, Paths.get(uploadDir + storageFileName), StandardCopyOption.REPLACE_EXISTING);
+            }
+        } catch (Exception e) {
+            System.out.println("Exception: " + e.getMessage());
+        }
+
+        Product product = new Product();
+        product.setName(productDto.getName());
+        product.setBrand(productDto.getBrand());
+        product.setCategory(productDto.getCategory());
+        product.setPrice(productDto.getPrice());
+        product.setDescription(productDto.getDescription());
+        product.setCreatedAt(createdAt);
+        product.setImageFileName(storageFileName);
+
+        repository.save(product);
+
+        return "redirect:/products";
+    }
+
+    @GetMapping("/edit")
+    public String showEditPage(Model model, @RequestParam int id) {
+        try {
+            Product product = repository.findById(id).get();
+            model.addAttribute("product", product);
+
+            ProductDto productDto = new ProductDto();
+            productDto.setName(product.getName());
+            productDto.setBrand(product.getBrand());
+            productDto.setCategory(product.getCategory());
+            productDto.setPrice(product.getPrice());
+            productDto.setDescription(product.getDescription());
+
+            model.addAttribute("productDto", productDto);
+        } catch (Exception e) {
+            System.out.println("Exception: " + e.getMessage());
+            return "redirect:/products";
+        }
+
+        return "products/EditProduct";
+    }
+
+    @PostMapping("/edit")
+    public String updateProduct(Model model, @RequestParam int id, @Valid @ModelAttribute ProductDto productDto, BindingResult result) {
+        try {
+            Product product = repository.findById(id).get();
+            model.addAttribute("product", product);
+
+            if (result.hasErrors()) {
+                return "products/EditProduct";
+            }
+        } catch (Exception e) {
+            System.out.println("Exception: " + e.getMessage());
+        }
         if (productDto.getImageFile().isEmpty()) {
             result.addError(new FieldError("productDto", "imageFile", "The image file is required"));
         }
